4082dac6dcad558baf7ae3bd2fa4155c7d4d6845
João Ferreira <jooaooferreira@gmail.com>
2017-02-15 15:58:32 +0000

    module 25

---
+++ module_25/content/js/counter.directive.js
@@ -0,0 +1,17 @@
+function counter() {
+    return {
+        scope: {},
+        restrict: 'E',
+        controller: 'CounterController as counter',
+        template: `
+            <div>
+                <input type="text" ng-model="counter.count">
+                <button type="button" ng-click="counter.decrement();">-</button>
+                <button type="button" ng-click="counter.increment();">+</button>
+            </div>`,
+    };
+}
+
+angular
+    .module('app')
+    .directive('counter', counter);
